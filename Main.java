e02f02e3fd67e9d64159ff2fd4aaef9ded286078
user 1 <user.1@education.merann.ru>
2018-06-02 13:26:46 +0300

    First commit

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
-import java.util.ArrayList;
+﻿import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
 	    List<Integer> col=new ArrayList<> ();
-        System.out.println("Здравствуйте!" +args[0]);
+        System.out.println("Здравствуйте" +args[0]);
     }
 }
